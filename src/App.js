c207430545dc47ada672338a482eacde61b7cff4
Oleksii Sanak <osanak@varteq.com>
2019-05-16 18:52:22 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h1>Learn React is started</h1>
         </a>
       </header>
     </div>
